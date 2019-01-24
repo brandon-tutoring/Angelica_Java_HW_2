611124b551a8eff57b60ddf3ca233f09b9a64657
Angelica Schell <Angelica@Angelicas-Air.lan1>
2019-01-23 21:48:09 -0500

    Finally done

--- Problem_2/main.java
+++ Problem_2/main.java
@@ -1,15 +1,14 @@
-// make sure to test if there is unicode character for the number
-// GRADE: 8/10 (B-)
+// GRADE: 10/10 (A)
 
 
 import java.util.Scanner;
 public class main {
 	
-	public static boolean isNumeric(String str)
-	{
-	    for (char c : str.toCharArray())
-	    {
-	        if (!Character.isDigit(c)) return false;
+	public static boolean isNumeric(String str) {
+	    for (char c : str.toCharArray()) {
+	        if (!Character.isDigit(c)) {
+	        	return false;
+	        }
 	    }
 	    return true;
 	}
@@ -18,50 +17,36 @@ public class main {
 		
 		/* Asks user for input and prints what it equals */
 		Scanner input = new Scanner(System.in);
-		System.out.println("Enter a unicode code point, and I'll print out a report.");
-		String amount = input.nextLine();
+		System.out.println("Enter a unicode code point, and I'll print out a report");
+		String user_input = input.nextLine();
 					
-		if (isNumeric(amount) == false) {
+		while (isNumeric(user_input) == false) {
 			System.out.println("That is not a number, please enter a number and try again.");
-			main(args);
+			user_input = input.nextLine();
 		}
-		else if (isNumeric(amount) == true) {
-			
-	
-			int amount_int = Integer.parseInt(amount);
-			char ch = (char) amount_int;
-			
-			System.out.print(amount + " represents the character " + "'"ch"'");
-			
-			/* Previous characters. */
-			int amount_minus_1 = amount_int - 1;
-			char ch_amount_minus_1 = (char) amount_minus_1;
-			System.out.print("\nThe previous character is " + "'"ch_amount_minus_1"'");
-			
-			
-			/* Conditional to tell what kind of character it is. */
-	
-			if (Character.isLetter(ch)) {
-				System.out.print("\nThe character is a letter."); 
-			}
-			else if (Character.isDigit(ch)) {
-				System.out.print("\nThe character is a number.");
-			}
-			
-			else {
-				System.out.print("\nThe characters is alphanumeric.");
-			 }
 		
-			
-			/* Next characters. */
-			int amount_plus_1 = amount_int + 1;
-			char ch_amount_plus_1 = (char) amount_plus_1;
-			System.out.println("\nThe next character is " + "'"ch_amount_plus_1"'");
-			
-			 
-			
-		};
+		int user_input_int = Integer.parseInt(user_input);
+		char ch = (char) user_input_int;
+		
+		System.out.print(user_input + " represents the character: " + "'" + ch + "'");
+		
+		/* Conditional to tell what kind of character it is. */
+		if (Character.isLetter(ch)) {
+			System.out.print("\nThe character is a letter."); 
+		} else if (Character.isDigit(ch)) {
+			System.out.print("\nThe character is a number.");
+		} else {
+			System.out.print("\nThe characters is alphanumeric.");
 		}
-	}
-
-
+		 
+		/* Next characters. */
+		int user_input_plus_1 = user_input_int + 1;
+		char ch_user_input_plus_1 = (char) user_input_plus_1;
+		System.out.print("\nThe next character is '" + ch_user_input_plus_1 + "'.");
+		
+		/* Previous characters. */
+		int user_input_minus_1 = user_input_int - 1;
+		char ch_user_input_minus_1 = (char) user_input_minus_1;
+		System.out.println("\nThe previous character is '" + ch_user_input_minus_1 + "'");
+	}	
+}

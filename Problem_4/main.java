98b022d18e5d61a0469a9c4e3a1854cd99865ade
Angelica Schell <Angelica@Angelicas-MacBook-Air.local>
2019-03-21 19:32:10 -0400

    I think the circle problem is done now

--- Problem_4/main.java
+++ Problem_4/main.java
@@ -1,13 +1,21 @@
-// do not include .class file on github (either add *.class or manually don't add it to commit)
-// why are you including package if no package is present and are running w terminal/sublime (?)
-
 // GRADE: 7/10
 
 
 import java.util.Scanner;
 import java.lang.*;
+import java.lang.Math; 
 
 public class main {
+
+	public static boolean isNumeric(String str) {
+	    for (char c : str.toCharArray()) {
+	        if (!Character.isDigit(c)) {
+	        	return false;
+	        }
+	    } 
+	    return true;
+	}
+
 
 	public static void main(String[] args) {
 		
@@ -18,9 +26,19 @@ public class main {
 		
 		// should include error checking for non valid inputs (i.e. not space seperated, not 3 values given, not integers given, etc.)
 		String[] circle_1_coords_split = circle_1_coords.split("\\s");
-		String circle_1_x_coord = circle_1_coords_split[0];
-		String circle_1_y_coord =circle_1_coords_split[1];
-		String circle_1_radii =circle_1_coords_split[2];
+		if (isNumerin(circle_1_coords_split[0]) == true){
+			String circle_1_x_coord = circle_1_coords_split[0];
+			String circle_1_y_coord =circle_1_coords_split[1];
+			String circle_1_radii =circle_1_coords_split[2];
+		} else {
+			System.out.println("That is not a number, please enter a number and try again.");
+			circle_1_x_coord = circle_1_coords_split[0];
+		}
+
+
+		if (isNumeric(circle_1_x_coord) == false)
+			System.out.println("That is not a number, please enter a number and try again.");
+			circle_1_x_coord = circle_1_coords_split[0];
 		
 		// minor note calling these ints in variable names but saving as double
 		double circle_1_x_coord_int = Double.parseDouble(circle_1_x_coord);
@@ -45,7 +63,7 @@ public class main {
 		/* Distance between 2 centers */
 		// not how you calculate distance between two points (look up distance formula to find how to calculate this correctly)
 		// i.e. https://www.baeldung.com/java-distance-between-two-points
-		double dist_btw_2_centers = (((circle_2_x_coord_int) - (circle_1_x_coord_int)) + ((circle_2_y_coord_int) - (circle_1_y_coord_int)));
+		double dist_btw_2_centers = Math.sqrt(((circle_2_x_coord_int) - (circle_1_x_coord_int))*((circle_2_x_coord_int) - (circle_1_x_coord_int))) + ((circle_2_y_coord_int) - (circle_1_y_coord_int))*((circle_2_y_coord_int) - (circle_1_y_coord_int));
 		/* Radius 1 minus radius 2 */
 		double radii_1_minus_radii_2 = (circle_1_radii_int - circle_2_radii_int);
 		/* Radius 1 plus radius 2 */

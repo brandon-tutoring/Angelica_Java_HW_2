f75b7aec2e3034675b3fc3275c3e832ef98c7d5f
Angelica Schell <Angelica@Angelicas-MacBook-Air.local>
2019-03-21 20:39:40 -0400

    Close to finishing

--- Problem_5/main.java
+++ Problem_5/main.java
@@ -2,8 +2,6 @@
 // GRADE: 0/10 (DOES NOT COMPILE !)
 
 
-
-
 import java.nio.charset.Charset;
 import java.util.Random;
 import java.util.Scanner;
@@ -21,57 +19,60 @@ public class main {
 		System.out.println("What kind of player do you want the computer to be?(h or c) \n(H)onest Olivia \n(C)heating Chelsea");
 		String kind_player = kind_player_input.nextLine();
 		
-		String[] choices = {"1", "2"};
-		
 		Scanner odd_even_input = new Scanner(System.in);
 		System.out.println("Choose odd or even (type the number corresponding to your choice) \n1- Odds \n2- Evens");
 		String odd_even = odd_even_input.nextLine();
 			
 		if (odd_even == 1){
-			System.out.println("You chose odds.")
+			System.out.println("You chose odds");
+		}else if (odd_even == 2) {
+			System.out.println("You chose evens");
 		}
-		else if (odd_even == 2) {
-			System.out.println("You chose evens.")
-		}
-			
+
 		Scanner fingers_input = new Scanner(System.in);
 		System.out.println("1.. 2.. 3... go! (How many fingers - 1 or 2)");
 		String fingers_amount = input.nextLine();
 		
-		if(kind_player == "h" of kind_player == "H") {
-			
+		if(kind_player == "h" or kind_player == "H"); {
+			String[] choices = {"1", "2"};
 			java.util.Random random = new java.util.Random();
-			int generatedString = random.nextInt(choices.length);
-			System.out.println(generatedString);
-			
-			
-					
+			int random_number = random.nextInt(choices.length);
 
-		else if(kind_player == "c" or kind_player == "C"){
-		
-			Scanner fingers_input = new Scanner(System.in);
-			System.out.println("1.. 2.. 3... go! (How many fingers - 1 or 2)");
-			String fingers_amount = input.nextLine();
-			
-			if(input2 == "odds"){
+			int total = random_number + fingers_amount
 
-				if(fingers_input == "1"){
-					System.out.println("Cheating computer cheats \nthe computer puts out 1 \nthe total is 2 \nyou lost!");
+			if(odd_even == "odds"){
+
+				if(fingers_amount == "1"){
+					System.out.println("The computer puts out " + random_number + "\nthe total is " + total + "\nyou " + );
+				}else if(fingers_amount == "2"){
+					System.out.println("The computer puts out " + random_number + "\nthe total is " + total + "\nyou " + );
 				}
-					
-				else if(num_input == "2"){
-					System.out.println("Cheating computer cheats \nthe computer puts out 2 \nthe total is 4 \nyou lost!");
+			}else if(odd_even == "evens"){
+			
+				if(fingers_amount == "1"){
+					System.out.println("The computer puts out " + random_number + "\nthe total is " + total + "\nyou " + );
+				}else if(fingers_amount == "2"){
+					System.out.println("The computer puts out " + random_number + "\nthe total is " + total + "\nyou " + );
 				}	
-					
-			else if(fingers_amount == "evens"){
+			}
+		}else if(kind_player == "c" or kind_player == "C"); {
 			
-				if(num_input == "1"){
-					System.out.println("Cheating computer cheats \nthe computer puts out 2 \nthe total is 3 \nyou lost!");
+			if(odd_even == "odds"){
+
+				if(fingers_amount == "1"){
+					System.out.println("Cheating computer cheats \nthe computer puts out 1 \nthe total is 2 \nyou lost!");
+				}else if(fingers_amount == "2"){
+					System.out.println("Cheating computer cheats \nthe computer puts out 2 \nthe total is 4 \nyou lost!");
 				}
-					
-				else if(num_input == "2"){
+			}else if(odd_even == "evens"){
+			
+				if(fingers_amount == "1"){
+					System.out.println("Cheating computer cheats \nthe computer puts out 2 \nthe total is 3 \nyou lost!");
+				}else if(fingers_amount == "2"){
 					System.out.println("Cheating computer cheats \nthe computer puts out 1 \nthe total is 2 \nyou lost!");
 				}	
 			}
+		}
 	};
 }
+

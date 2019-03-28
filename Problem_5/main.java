e7ddfa389ac40a38dfa8417872c35d3c2c565be9
Angelica Schell <Angelica@Angelicas-Air.lan1>
2019-03-28 19:55:52 -0400

    Just have to finish the error catching

--- Problem_5/main.java
+++ Problem_5/main.java
@@ -1,7 +1,3 @@
-// dont include .class file (same as previous problem)
-// GRADE: 0/10 (DOES NOT COMPILE !)
-
-
 import java.nio.charset.Charset;
 import java.util.Random;
 import java.util.Scanner;
@@ -18,74 +14,83 @@ public class main {
 		Scanner kind_player_input = new Scanner(System.in);
 		System.out.println("What kind of player do you want the computer to be?(h or c) \n(H)onest Olivia \n(C)heating Chelsea");
 		String kind_player = kind_player_input.nextLine();
+		System.out.println(kind_player);
 		
 		Scanner odd_even_input = new Scanner(System.in);
 		System.out.println("Choose odd or even (type the number corresponding to your choice) \n1- Odds \n2- Evens");
 		String odd_even = odd_even_input.nextLine();
 			
-		if (odd_even == 1){
+		if (odd_even.equals("1")){
 			System.out.println("You chose odds");
-		}else if (odd_even == 2) {
+		}else if (odd_even.equals("2")) {
 			System.out.println("You chose evens");
 		}
 
 		Scanner fingers_input = new Scanner(System.in);
 		System.out.println("1.. 2.. 3... go! (How many fingers - 1 or 2)");
-		String fingers_amount = input.nextLine();
+		String fingers_amount = fingers_input.nextLine();
+		int fingers_amount_int = Integer.parseInt(fingers_amount);
 		
-		if(kind_player == "h" or kind_player == "H") {
+		if(kind_player.equals("h") || kind_player.equals("H")) {
+
 			String[] choices = {"1", "2"};
 			java.util.Random random = new java.util.Random();
 			int random_number = random.nextInt(choices.length);
 
-			int total = random_number + fingers_amount;
-			if (total % 2 = 0){
-				String result = "even";
+			int total = random_number + fingers_amount_int;
+			String result = "";
+			if (total % 2 == 0){
+				result = "even";
 			} else if (total % 2 != 0) {
-				String result = "odd";
+				result = "odd";
 			}
-
-			if(odd_even == "odds"){
-
-				if (result == "odd"){
-					String win_lose = "won";
-				} else if (result == "even"){
-					String win_lose = "lose";
+			System.out.println(odd_even + "odd even");
+			if(odd_even.equals("1")){
+				String win_lose = "";
+				System.out.println("odd even loop if");
+				if (result.equals("odd")){
+					win_lose = "won";
+				} else if (result.equals("even")){
+					win_lose = "lose";
 				}
 
-				if(fingers_amount == "1"){
+				if(fingers_amount.equals("1")){
 					System.out.println("The computer puts out " + random_number + "\nthe total is " + total + "\nyou " + win_lose + "!");
-				}else if(fingers_amount == "2"){
+				}else if(fingers_amount.equals("2")){
 					System.out.println("The computer puts out " + random_number + "\nthe total is " + total + "\nyou " + win_lose + "!");
 				}
-			}else if(odd_even == "evens"){
-
-				if (result == "odd"){
-					String win_lose = "lose";
-				} else if (result == "even"){
-					String win_lose = "won";
+			}else if(odd_even.equals("2")){
+				System.out.println("odd even loop elseif");
+				String win_lose = "";
+				if (result.equals("odd")){
+					win_lose = "lose";
+				} else if (result.equals("even")){
+					win_lose = "won";
 				}
 			
-				if(fingers_amount == "1"){
+				if(fingers_amount.equals("1")){
 					System.out.println("The computer puts out " + random_number + "\nthe total is " + total + "\nyou " + win_lose + "!");
-				}else if(fingers_amount == "2"){
+				}else if(fingers_amount.equals("2")){
 					System.out.println("The computer puts out " + random_number + "\nthe total is " + total + "\nyou " + win_lose + "!");
 				}	
 			}
-		}else if(kind_player == "c" or kind_player == "C") {
-			
-			if(odd_even == "odds"){
 
-				if(fingers_amount == "1"){
+		}else if (kind_player.equals("c") || kind_player.equals("C")) {
+
+			if(odd_even.equals("1")){
+				System.out.println("Inside for loop odd");
+
+				if(fingers_amount.equals("1")){
 					System.out.println("Cheating computer cheats \nthe computer puts out 1 \nthe total is 2 \nyou lost!");
-				}else if(fingers_amount == "2"){
+				}else if(fingers_amount.equals("2")){
 					System.out.println("Cheating computer cheats \nthe computer puts out 2 \nthe total is 4 \nyou lost!");
 				}
-			}else if(odd_even == "evens"){
-			
-				if(fingers_amount == "1"){
+			}else if(odd_even.equals("2")){
+				System.out.println("Inside for loop even");
+
+				if(fingers_amount.equals("1")){
 					System.out.println("Cheating computer cheats \nthe computer puts out 2 \nthe total is 3 \nyou lost!");
-				}else if(fingers_amount == "2"){
+				}else if(fingers_amount.equals("2")){
 					System.out.println("Cheating computer cheats \nthe computer puts out 1 \nthe total is 2 \nyou lost!");
 				}	
 			}
